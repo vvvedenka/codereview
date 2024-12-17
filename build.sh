ceccc0e8e248f59e9737b3f50f9eb32665faf59a
Nastya <145253167+vvvedenka@users.noreply.github.com>
2024-12-17 14:30:20 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
-# Загружаем переменные из .env
 export $(grep -v '^#' .env | xargs)
-
 echo "Собираем и запускаем проект через Docker Compose..."
 docker-compose down --volumes
 docker-compose up --build -d
-
 echo "Ожидаем запуска PostgreSQL..."
-# Ожидаем, пока PostgreSQL не станет доступен
 until docker exec telegram_bot_db pg_isready -U vvvedenka > /dev/null 2>&1; do
   echo "PostgreSQL ещё не готов, подождите 5 секунд..."
   sleep 5
 done
-
 echo "PostgreSQL готов. Выполняем скрапинг данных..."
-
 docker exec -it dictionary_unusual_bot  python3 /app/scraper.py
-
 echo "Проект запущен. Telegram-бот работает!"
