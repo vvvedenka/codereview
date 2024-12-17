2dcd02f17aa44d0c5e50afdfe3fba2c2bc03fade
Nastya <145253167+vvvedenka@users.noreply.github.com>
2024-12-17 17:56:53 +0500

    update

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://vvvedenka:1230@db:5432/vvvedenka")
 
 engine = create_engine("postgresql://vvvedenka:1230@db:5432/vvvedenka")
-Session = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 def get_db_session():
     return SessionLocal()
